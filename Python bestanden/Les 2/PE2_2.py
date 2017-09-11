0f2db4be4d814229f317dd6ac78772d5715f6781
chester <cvdelang@gmail.com>
2017-09-11 10:32:18 +0200

    Finished

---
+++ Python bestanden/Les 2/PE2_2.py
@@ -0,0 +1,8 @@
+cijferICOR = 7
+cijferCSN = 6
+cijferPROG = 8
+totaal = cijferCSN + cijferICOR + cijferPROG
+bedrag = str(totaal * 30)
+gemiddeld = str(totaal / 3)
+
+print('Mijn cijfers' + '(gemiddeld een' +' '+ gemiddeld + '' + ') Leveren een beloning op van' + ' ' + bedrag + ' ' + 'op !')
